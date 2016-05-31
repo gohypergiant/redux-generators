c5bb1e83aebbad59acd92e0e3a7a11b7e0ddf043
Brandon Pierce <brandon@brandonjpierce.com>
2016-05-31 19:25:34 -0400

    Update success messages, add in name flag to single file make commands, normalize make folder name

--- src/commands/make-action.js
+++ src/commands/make-action.js
@@ -2,6 +2,8 @@ const program = require('commander');
 const path = require('path');
 const template = require('lodash.template');
 const snakeCase = require('lodash.snakecase');
+const lowercase = require('lodash.tolower');
+const kebab = require('lodash.kebabcase');
 const uppercase = require('lodash.toupper');
 const utils = require('../utils');
 const paths = require('../paths');
@@ -9,12 +11,16 @@ const paths = require('../paths');
 program
   .command('make:action')
   .option('--items [list]', 'Add action items', utils.list, ['testAction'])
+  .option('--name [name]', 'Set filename for actions file', 'actions')
   .action(options => {
     const insertPath = path.join(options.parent.root, options.parent.path);
+    const fileName = lowercase(kebab(options.name));
 
     utils.info('Creating action...');
-    utils.exists('actions.js')
-      .then(() => utils.exit('A action in this directory already exists.'))
+    utils.exists(`${fileName}.js`)
+      .then(() => utils.exit(
+        `Actions file with filename "${fileName}.js" already exists.`
+      ))
       .catch(() => utils.read(paths.actionStub, 'utf8'))
       .then(content => Promise.resolve(
         template(content)({
@@ -24,7 +30,9 @@ program
             .map(uppercase),
         })
       ))
-      .then(content => utils.write(`${insertPath}actions.js`, content))
-      .then(() => utils.success('Action successfully created!'))
+      .then(content => utils.write(`${insertPath}${fileName}.js`, content))
+      .then(() => utils.success(
+        `Actions file successfully created! ==> "${insertPath}${fileName}.js"`
+      ))
       .catch(utils.exit);
   });
